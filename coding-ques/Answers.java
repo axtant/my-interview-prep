83c920aed74a84a23d293704cffb838146a71308
axtant <abhishekgordex10@gmail.com>
2026-05-19 12:08:35 +0530

    Q.1 Brut Solution

--- coding-ques/Answers.java
+++ coding-ques/Answers.java
@@ -1,21 +1,40 @@
-import java.util.Arrays;
+
 
 public class Answers {    
    public static void main(String[] args) {
 
     //Inputs
-    int[] nums = {2,7,11,15};
-    int target = 9;
+    int[] nums = {3,0,1};
 
 
     //Funtion 
-    System.out.println(Arrays.toString(twoSum(nums, target)));
+    System.out.println(missingNumber(nums));
 
+    }
+
+    /*Q.1 https://leetcode.com/problems/missing-number 
+            Brut force 
+            Time O(n^2)
+            Space O(1) */
+    public static int missingNumber(int[] nums){
+       for (int i = 0; i < nums.length; i++) {
+            boolean found = false;
+             for (int j = 0; j < nums.length; j++) {
+                if (nums[j] == i){
+                    found = true;
+                    break;
+                }
+             }
+             if(!found) return i;   
+       }
+        return 1;
     }
 
 
     /* Q3. https://leetcode.com/problems/two-sum/  
-            Solution brut force Time O(n^2) Space O(1) */
+            Solution brut force
+            Time O(n^2)
+            Space O(1) */
     public static int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j < nums.length; j++) {
